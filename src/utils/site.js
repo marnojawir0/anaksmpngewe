4151b3129665dd7d568dd7682698f6126dda2479
marnojawir0 <marnojawir0@gmail.com>
2025-07-09 23:39:27 +0700

    Update site.js

--- src/utils/site.js
+++ src/utils/site.js
@@ -1,5 +1,5 @@
 export const url = 'https://anaksmpngewe.pages.dev';
-export const nama = 'Anak Kecil Ngewe';
+export const nama = 'Anak Smp Ngewe';
 export const desk = 'Video Bokep Indo Terbaru Bocil Ngentot Jilbab Smp Mama Viral Sma Cindo Tante';
 export const terbit = '2025-01-25T00:01:03Z';
 
